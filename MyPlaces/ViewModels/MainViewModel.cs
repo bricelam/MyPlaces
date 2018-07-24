140d7fcee7479b59c8053fd6eff5da568fda4b3e
Brice Lambson <bricelam@users.noreply.github.com>
2018-07-23 20:31:56 -0700

    Try to get the user's location

--- MyPlaces/ViewModels/MainViewModel.cs
+++ MyPlaces/ViewModels/MainViewModel.cs
@@ -1,13 +1,17 @@
-﻿using System.Windows.Input;
+﻿using System;
+using System.Windows.Input;
 using GalaSoft.MvvmLight;
 using GalaSoft.MvvmLight.Command;
 using GeoAPI.Geometries;
 using Microsoft.Extensions.Configuration;
+using NetTopologySuite.Geometries;
 
 namespace MyPlaces.ViewModels
 {
     class MainViewModel : ViewModelBase
     {
+        IPoint _center = new Point(-98.5795, 39.828175);
+
         public MainViewModel(IConfiguration configuraiton)
         {
             BingMapsKey = configuraiton["BingMapsKey"];
@@ -20,9 +24,26 @@ namespace MyPlaces.ViewModels
                 x => MouseLeftButtonUp(x.position, x.handleable));
             MouseLeftButtonDownCommand = new RelayCommand<(IPoint position, Handleable handleable)>(
                 x => MouseLeftButtonDown(x.position, x.handleable));
+
+            using (var watcher = new System.Device.Location.GeoCoordinateWatcher())
+            {
+                if (watcher.TryStart(false, TimeSpan.FromMilliseconds(1000)))
+                {
+                    var location = watcher.Position.Location;
+                    if (!location.IsUnknown)
+                    {
+                        Center = new Point(location.Longitude, location.Latitude);
+                    }
+                }
+            }
         }
 
         public string BingMapsKey { get; }
+        public IPoint Center
+        {
+            get => _center;
+            set => Set(nameof(Center), ref _center, value);
+        }
 
         public ICommand ViewChangeEndCommand { get; }
         public ICommand MouseMoveCommand { get; }
